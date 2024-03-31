d895f7c868dacf2fdc7a211c7661747f307f3352
andrewtroicki <atroicki@icloud.com>
2024-03-31 18:39:21 +1100

    first test passed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const thankYouMessages = writeCards(names);
+console.log(thankYouMessages);
